07e30b25d01dacef411841bbe778ce12de667c79
Marcelo P. Patrício <marc3lo.patricio.25@gmail.com>
2025-10-27 10:03:58 -0300

    teste

--- project_management/src/App.tsx
+++ project_management/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import FormEditProjeto from './components/FormEditProjeto'
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <FormEditProjeto />
     </>
   )
 }
