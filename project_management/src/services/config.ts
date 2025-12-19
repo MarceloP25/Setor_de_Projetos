267ee4f427f8cbf3045e1de0fce8a96f6fbe2d8c
Karla C. Ferreira <kferreira7581@gmail.com>
2025-12-19 12:28:33 +0000

    [fix] - add .env

--- project_management/src/services/config.ts
+++ project_management/src/services/config.ts
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3wPLES-clrm92-EysVwnr_quFY9MpfAo",
-  authDomain: "reserva-setor-projetos.firebaseapp.com",
-  projectId: "reserva-setor-projetos",
-  storageBucket: "reserva-setor-projetos.firebasestorage.app",
-  messagingSenderId: "792713377331",
-  appId: "1:792713377331:web:fd67dcd411dc840b1a5546",
-  measurementId: "G-7HSDVS3YNT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
